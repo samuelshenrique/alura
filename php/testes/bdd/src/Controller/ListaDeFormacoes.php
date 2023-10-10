1bf1a1c7274afb570e12a9345dd1cb909073eb60
Samuel Santos Henrique <samuel.santos99@hotmail.com>
2023-10-09 21:35:42 -0300

    feature: realizando criação de páginas de formação (WIP)

---
+++ php/testes/bdd/src/Controller/ListaDeFormacoes.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Alura\Armazenamento\Controller;
+
+use Alura\Armazenamento\Entity\Formacao;
+use Alura\Armazenamento\Helper\HtmlViewTrait;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\EntityRepository;
+use Nyholm\Psr7\Response;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+
+class ListaDeFormacoes implements RequestHandlerInterface
+{
+    use HtmlViewTrait;
+
+    private EntityRepository $formacoesRepository;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->formacoesRepository = $entityManager->getRepository(Formacao::class);
+    }
+
+    public function handle(ServerRequestInterface $request): ResponseInterface
+    {
+        $formacoes = $this->formacoesRepository->findBy($request->getQueryParams(), ['descricao' => 'ASC']);
+        $titulo = 'Listagem de Formações';
+
+        $html = $this->getHtmlFromTemplate(
+            'formacoes/lista.php',
+            compact($formacoes, $titulo)
+        );
+
+        return new Response(200, [], $html);
+    }
+}
---
+++ php/testes/bdd/view/formacoes/lista.php
@@ -0,0 +1,27 @@
+<?php require __DIR__ . '/../inicio-html.php'; ?>
+
+<div class="jumbotron">
+    <h1><?= $titulo; ?></h1>
+</div>
+
+<a href="/nova-formacao" class="btn btn-primary mb-2">Nova Formação</a>
+
+<ul class="list-group">
+    <?php foreach ($formacoes as $formacao) : ?>
+        <li class="list-group-item d-flex justify-content-between">
+            <?= $formacao->getDescricao(); ?>
+
+            <span>
+                <a href="/editar-formacao?id=<?= $formacao->getId(); ?>" class="btn btn-sm btn-info">
+                    Editar
+                </a>
+
+                <a href="/excluir-curso?id=<?= $formacao->getId(); ?>" class="btn btn-sm btn-danger">
+                    Excluir
+                </a>
+            </span>
+        </li>
+    <?php endforeach; ?>
+</ul>
+
+<?php require __DIR__ . '/../fim-html.php'; ?>
