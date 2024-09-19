1def973a01b89a1c166534af5b1581fa39a17bbc
Samuel Santos Henrique <samuel.santos99@hotmail.com>
2024-09-19 17:21:18 -0300

    alura+: useEffect e useState na Alura+

---
+++ react/use-state-effect/src/App.js
@@ -0,0 +1,37 @@
+import './App.css';
+import { useEffect, useState } from 'react';
+
+function App() {
+  const [racas, setRacas] = useState([]);
+  const [busca, setBusca] = useState('');
+
+  useEffect(() => {
+    fetch('http://localhost:8080/doguinhos')
+      .then((resposta) => resposta.json())
+      .then(dados => {
+        setRacas(dados)
+      });
+  }, []);
+
+  useEffect(() => {
+    if (busca && busca.length > 3) {
+      fetch('http://localhost:8080/doguinhos?nome=' + busca)
+        .then((resposta) => resposta.json())
+        .then(dados => {
+          setRacas(dados)
+        });
+    }
+  }, [busca]);
+
+  return (
+    <div className="App">
+      <h1>Bem-vindo aos doguinhos!</h1>
+      <input placeholder='Buscar por raça' onChange={evento => setBusca(evento.target.value)} />
+      <ul>
+        {racas.map(raca => <li key={raca.nome}>{raca.nome}</li>)}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
