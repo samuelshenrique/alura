ca5f665f4c7e3951822a45951762c80d3407e4f2
Samuel Santos Henrique <samuel.santos99@hotmail.com>
2025-03-14 11:39:34 -0300

    course: Praticando React

---
+++ react/cinetag/src/componentes/Rodape/index.js
@@ -0,0 +1,11 @@
+import styles from './Rodape.module.css';
+
+function Rodape() {
+    return (
+        <footer className={styles.container}>
+            <p className={styles.texto}>Desenvolvido por Alura.</p>
+        </footer>
+    )
+}
+
+export default Rodape;
---
+++ react/cinetag/src/pages/Inicio/index.js
@@ -0,0 +1,13 @@
+import Cabecalho from "componentes/Cabecalho";
+import Rodape from "componentes/Rodape";
+
+function Inicio() {
+    return (
+        <>
+        <Cabecalho />
+        <Rodape />
+        </>
+    );
+}
+
+export default Inicio;
