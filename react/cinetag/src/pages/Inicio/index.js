8d4ddcfe685b298b45b4ef8caaa51f2785b7d414
Samuel Santos Henrique <samuel.santos99@hotmail.com>
2025-03-19 08:50:32 -0300

    course: Curso praticando React (WIP)

--- react/cinetag/src/pages/Inicio/index.js
+++ react/cinetag/src/pages/Inicio/index.js
@@ -1,10 +1,18 @@
+import Banner from "componentes/Banner";
 import Cabecalho from "componentes/Cabecalho";
+import Card from "componentes/Card";
 import Rodape from "componentes/Rodape";
+import Titulo from "componentes/Titulo";
 
 function Inicio() {
     return (
         <>
         <Cabecalho />
+        <Banner imagem="home" />
+        <Titulo>
+            <h1>Um lugar para guardar seus vídeos e filmes!</h1>
+        </Titulo>
+        <Card id='1' titulo='Gato' capa='https://thecatapi.com/api/images/get?format=src&type=png&width=200&height=200' />
         <Rodape />
         </>
     );
