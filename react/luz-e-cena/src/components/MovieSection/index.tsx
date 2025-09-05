b8e02c19b484b786abb75dcc4b585865de1df00c
Samuel Santos Henrique <samuel.santos99@hotmail.com>
2025-09-05 10:24:16 -0300

    course: (WIP) Typescript com React

--- react/luz-e-cena/src/components/MovieSection/index.tsx
+++ react/luz-e-cena/src/components/MovieSection/index.tsx
@@ -5,24 +5,32 @@ import Button from '../Button'
 import { FaSearch } from 'react-icons/fa'
 import MovieList from '../MovieList'
 import useFetchMovies from '../../hooks/useFetchMovies'
+import useFilterMovies from '../../hooks/useFilterMovies'
 
 function MovieSection() {
     const { movies, error, isLoading} = useFetchMovies();
+    const { searchTerm, setSearchTerm, filteredMovies, handleSearch } = useFilterMovies(movies);
 
     return (
         <main>
             <section className={styles.container}>
                 <Fieldset variant='secondary'>
-                    <InputText placeholder='Buscar filmes...' />
-                    <Button variant='icon'>
+                    <InputText
+                        placeholder='Buscar filmes...'
+                        value={searchTerm}
+                        onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+                            setSearchTerm(event.target.value)}
+                    />
+                    <Button variant='icon' onClick={handleSearch}>
                         <FaSearch />
                     </Button>
                 </Fieldset>
 
                 <h1 className={styles.titulo}>Em cartaz</h1>
-                <p>error</p>
+                {isLoading && <p>Carregando filmes...</p>}
+                {error && <p className={styles.error}>{error}</p>}
                 <p>{isLoading ? 'LOADING' : 'SUCESSO'}</p>
-                <MovieList movies={movies} />
+                <MovieList movies={filteredMovies} />
             </section>
         </main>
     )
--- react/luz-e-cena/src/hooks/useFetchMovies.ts
+++ react/luz-e-cena/src/hooks/useFetchMovies.ts
@@ -24,7 +24,7 @@ const useFetchMovies = () => {
 
     useEffect(() => {
         fetchMovies();
-    });
+    }, []);
 
     return {
         movies,
