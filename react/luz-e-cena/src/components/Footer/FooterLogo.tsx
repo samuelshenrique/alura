2cfb77890029d2923149847044ff1ba43d3339df
Samuel Santos Henrique <samuel.santos99@hotmail.com>
2025-09-09 18:50:55 -0300

    fix: (WIP) React com Typescript

---
+++ react/luz-e-cena/src/components/Footer/FooterLogo.tsx
@@ -0,0 +1,13 @@
+import Logo from "../Logo"
+import styles from "./Footer.module.css"
+
+const FooterLogo = () => {
+  return (
+    <div>
+        <Logo src="./logo.png" alt="Logo" />
+        <p>Onde a realidade encontra a fantasia</p>
+    </div>
+  )
+}
+
+export default FooterLogo
